a789b74d66b83458ffac33ccb1f378fe87a43a62
lpdft <772346730@qq.com>
2020-08-19 15:27:41 +0800

    [ruyue] feat:click button can get datas

--- app/src/main/java/com/ruyue/network/NetworkActivity.java
+++ app/src/main/java/com/ruyue/network/NetworkActivity.java
@@ -5,22 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NetworkActivity extends AppCompatActivity {
-    private Button getData;
+    private Button getDataBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network);
 
-        getData = findViewById(R.id.get_data);
+        getDataBtn = findViewById(R.id.get_data);
         setListeners();
     }
 
+
+
     private void setListeners() {
         OnClick onclick = new OnClick();
-        getData.setOnClickListener(onclick);
+        getDataBtn.setOnClickListener(onclick);
 
 
     }
@@ -30,10 +44,40 @@ public class NetworkActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch(v.getId()) {
                 case R.id.get_data:
-
+                    getData();
                     break;
             }
         }
+    }
+
+    private void getData() {
+        String url = "https://twc-android-bootcamp.github.io/fake-data/data/default.json";
+        final Request request = new Request.Builder().url(url).build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Call call = okHttpClient.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                final String result = Objects.requireNonNull(response.body()).string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
 
