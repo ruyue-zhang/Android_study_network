b3df6c255896fca65ac5b823a8c1f2a76de1e2fc
lpdft <772346730@qq.com>
2020-08-19 11:08:15 +0800

    [ruyue] feat:add get datas button

--- app/src/main/java/com/ruyue/network/NetworkActivity.java
+++ app/src/main/java/com/ruyue/network/NetworkActivity.java
@@ -3,12 +3,36 @@ package com.ruyue.network;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NetworkActivity extends AppCompatActivity {
+    private Button getData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network);
+
+        getData = findViewById(R.id.get_data);
+        setListeners();
+    }
+
+    private void setListeners() {
+        OnClick onclick = new OnClick();
+        getData.setOnClickListener(onclick);
+
+
+    }
+
+    private class OnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.get_data:
+                    break;
+            }
+        }
     }
 }
+
