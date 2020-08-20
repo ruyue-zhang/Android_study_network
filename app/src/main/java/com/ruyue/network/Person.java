289aa7145adeec2b318a61ddbf27d3f0fad56b4c
lpdft <772346730@qq.com>
2020-08-20 15:33:19 +0800

    [ruyue] feat:add annotation in Person class

--- app/src/main/java/com/ruyue/network/Person.java
+++ app/src/main/java/com/ruyue/network/Person.java
@@ -1,12 +1,24 @@
 package com.ruyue.network;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Person {
+
+    @PrimaryKey
+    @ColumnInfo(name = "name")
     private String name;
+
+    @ColumnInfo(name = "avatar")
     private String avatar;
 
     public Person() {
     }
 
+    @Ignore
     public Person(String name, String avatar) {
         this.name = name;
         this.avatar = avatar;
